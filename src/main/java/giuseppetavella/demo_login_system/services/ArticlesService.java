b70a2b6521e4e11f0368f8a84ff306dd8ab457c4
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-22 19:13:42 +0200

    find articles by user: start implement

--- src/main/java/giuseppetavella/demo_login_system/services/ArticlesService.java
+++ src/main/java/giuseppetavella/demo_login_system/services/ArticlesService.java
@@ -6,8 +6,13 @@ import giuseppetavella.demo_login_system.exceptions.NotFoundException;
 import giuseppetavella.demo_login_system.exceptions.UnauthorizedException;
 import giuseppetavella.demo_login_system.repositories.ArticlesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -26,6 +31,22 @@ public class ArticlesService {
         return articlesRepository.findById(articleId).orElseThrow(() -> new NotFoundException(articleId, "article"));
     }
 
+
+    /**
+     * Get articles of the given user.
+     */
+    public Page<Article> findByUser(User user, int page, int size, String sortBy) {
+        // / the size of each page (how many elements in each page)
+        int finalSize = Math.min(10, size);
+        // the page number
+        int finalPage = Math.max(0, page);
+        // page is the function that will get translated to SQL,
+        // that will in turn filter the result set
+        Pageable pageable = PageRequest.of(finalPage, finalSize, Sort.by(sortBy));
+        return this.articlesRepository.findByUser(user, pageable);
+    }
+
+    
     /**
      * Add an article.
      * Checks if the user exists.
