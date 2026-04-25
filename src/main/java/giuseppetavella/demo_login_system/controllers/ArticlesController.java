c6dc5435d55aeeea8a10519244cbeeb3a6338605
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-25 22:36:34 +0200

    change articles controller to / (without /me)

--- src/main/java/giuseppetavella/demo_login_system/controllers/ArticlesController.java
+++ src/main/java/giuseppetavella/demo_login_system/controllers/ArticlesController.java
@@ -38,7 +38,7 @@ public class ArticlesController {
     /**
      * Get my articles.
      */
-    @GetMapping("/")
+    @GetMapping
     public Page<ArticleToSendDTO> findOwnArticles(@AuthenticationPrincipal User currentUser, 
                                                   @RequestParam(defaultValue = "0") int page,
                                                   @RequestParam(defaultValue = "10") int size,
